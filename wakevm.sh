c779217d4c4f115cd470efa22f764816a85b41cb
Ignacio Astaburuaga <13954480+nachobacanful@users.noreply.github.com>
2025-04-13 03:36:07 -0700

    Update wakevm.sh

--- wakevm.sh
+++ wakevm.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# WakeVM - Wake-on-LAN listener for Proxmox guests
-# Created by nachobacanful 
-# https://github.com/nachobacanful/wakevm.git
+# wakevm.sh - Wake-on-LAN listener for Proxmox VMs and containers
+# Copyright (C) 2024 Ignacio Astaburuaga
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # Configuration
 declare -A mac_lookup
